8f58880a8551348a021f282bdd234e410b3650c7
lukas <llbrower@princeton.edu>
2016-04-19 20:54:20 -0400

    Finished preprocessing, added tfidf matrix calculation

--- process.py
+++ process.py
@@ -2,15 +2,24 @@ import sys
 import re
 import json
 import nltk
+import string
 from nltk.corpus import stopwords
+from nltk.stem.porter import *
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 def main():
 
     raw_tweets = open(sys.argv[1])
     processed_tweets = process(raw_tweets)
+    processed_tweets = [tweet for tweet in processed_tweets if tweet != []]
+    tfidf = TfidfVectorizer(tokenizer=lambda i:i, lowercase=False)
+    tfs = tfidf.fit_transform(processed_tweets)
 
-    for tweet in processed_tweets:
-        print (tweet)
+    print(tfs)
+
+    #for tweet in processed_tweets:
+    #    print (tweet)
 
 def process(raw_tweets):
     tweets = []
@@ -32,27 +41,53 @@ def process(raw_tweets):
             tweet_texts.append(tweet['text'])
 
     tokenized = tokenize(tweet_texts)
-    processed = remove_stopwords(tokenized)
-    return processed
+    stop_removed = remove_stopwords(tokenized)
+    #stemmed = stem(stop_removed)
+    lemmatized = lemmatize(stop_removed)
+    return lemmatized
 
 def tokenize(tweets):
     tokenizer = nltk.TweetTokenizer()
     tweets_tokenized = []
     for text in tweets:
-        clean_text = re.sub(r"http\S+", "", text)
-        tweets_tokenized.append(tokenizer.tokenize(clean_text))
+        cleaned = clean_text(text)
+        tweets_tokenized.append(tokenizer.tokenize(cleaned))
     return tweets_tokenized
+
+def clean_text(text):
+    strip_urls = re.sub(r"http\S+", "", text)
+    strip_punctuation = ''.join(char for char in strip_urls if char not in string.punctuation)
+    clean_text = strip_punctuation.lower()
+    return clean_text
 
 
 def remove_stopwords(tweets):
     stop = set(stopwords.words('english'))
     # add some tweet-specific terms for stopword list
-    stop.update(('RT', ':', ',', '.'))
+    stop.update(('rt', ':', ',', '.'))
     tweets_stop = []
     for tweet in tweets:
         tweet_trimmed = [token for token in tweet if token not in stop]
         tweets_stop.append(tweet_trimmed)
     return tweets_stop
+
+def stem(tweets):
+    stemmer = PorterStemmer()
+    stemmed_tweets = []
+    for tweet in tweets:
+        # do not stem user mentions
+        stemmed = [stemmer.stem(token) if token[0] != '@' else token for token in tweet]
+        stemmed_tweets.append(stemmed)
+    return stemmed_tweets
+
+def lemmatize(tweets):
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_tweets = []
+    for tweet in tweets:
+        # do not stem user mentions
+        lemmatized = [lemmatizer.lemmatize(token) if token[0] != '@' else token for token in tweet]
+        lemmatized_tweets.append(lemmatized)
+    return lemmatized_tweets
 
 if __name__ == "__main__":
     main()
